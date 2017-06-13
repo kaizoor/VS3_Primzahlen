13256949ca8ab25d7cc1b0da81938385476a59ab
kaizoor <kai.schultz9612@gmail.com>
2017-06-13 21:54:36 +0200

    erste kack versuch

--- src/primeClient.java
+++ src/primeClient.java
@@ -1,4 +1,46 @@
-public class primeClient{
+package VS3_Primzahlen.src;
 
+import javax.xml.ws.Response;
+import java.net.*;
+import java.util.*;
+import java.xml.soap;
 
+public class primeClient {
+    public static void main(String[] args) {
+        String sServiceUrl = "http://localhost/soap/servlet/rpcrouter";
+        String sServiceUri = "urn:primeServer";  // must match primeServer.xml
+        String sMethodName = "getMessage";
+        String sInput = (0 < args.length) ? args[0] : "MeinName";
+        Response resp;
+
+        try {
+            Vector params = new Vector();
+            params.addElement(new Parameter("sInput", sInput.getClass(),
+                    sInput, null));
+            Call call = new Call();
+            call.setTargetObjectURI(sServiceUri);
+            call.setMethodName(sMethodName);
+            call.setEncodingStyleURI(Constants.NS_URI_SOAP_ENC);
+            call.setParams(params);
+            resp = call.invoke(new URL(sServiceUrl), sServiceUri);
+        } catch (Exception ex) {
+            System.err.println("Error while calling '" + sMethodName + "':");
+            System.err.println(ex.getMessage());
+            return;
+        }
+
+        if (resp.generatedFault()) {
+            System.out.println("Call to '" + sMethodName + "' returned a fault:");
+            System.err.println(resp.getFault());
+        } else {
+            if (null != resp.getReturnValue()) {
+                Object result = resp.getReturnValue().getValue();
+                if (null != result) {
+                    System.out.println("'" + sMethodName + "' returned an object of "
+                            + result.getClass() + ": ");
+                    System.out.println(result);
+                }
+            }
+        }
+    }
 }
--- src/primeServer.java
+++ src/primeServer.java
@@ -1,33 +1,10 @@
 
-package com.tutego.insel.ws;
+package VS3_Primzahlen.src;
 
-import javax.jws.*;
-import javax.jws.soap.SOAPBinding;
-import javax.swing.JOptionPane;
-import javax.xml.ws.Endpoint;
-
-
-@WebService(name = "VerteilteSysteme")
-@SOAPBinding(style = SOAPBinding.Style.RPC)
-public class primeServer {
-
-    @WebMethod
-    public String hello(String name) {
-        return "Hello " + name + "!";
-    }
-
-    @WebMethod(operationName = "body-mass-index")
-    @WebResult(name = "your-bmi")
-    public double bmi(@WebParam(name = "height") double height,
-                      @WebParam(name = "weight") double weight) {
-        return weight / ((height * height) / 10000);
-    }
-
-
-    public static void main(String[] args) {
-        Endpoint prime = Endpoint.publish("http://localhost:8080/services",
-                new primeServer());
-        JOptionPane.showMessageDialog(null, "Server beenden");
-        prime.stop();
+public class primeServer
+{
+    public String getMessage( String sCallerName )
+    {
+        return "Hello " + sCallerName + "!";
     }
 }
