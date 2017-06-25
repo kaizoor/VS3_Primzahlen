697f49498e32bf952461deb0b672111d9097e370
THOMAS\Thomas <thomas.winkler121@gmail.com>
2017-06-25 17:27:24 +0200

    commit wegen fehlerhafter Datei

--- src/wsclient/Client.java
+++ src/wsclient/Client.java
@@ -3,12 +3,12 @@ package wsclient;
 /**
  * Created by Thomas on 25.06.2017.
  */
-public class primeClient {
+public class Client {
 
     public static void main(String[] args)
     {
         PrimeServiceService service = new PrimeServiceService();
-        PrimeService primeclient = service.getPrimeServicePort();
-        primeclient.primzahl();
+        PrimeService primeService = service.getPrimeServicePort();
+        primeService.primzahl();
     }
 }
