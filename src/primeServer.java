9d28cfe693c9e35bc2ba5f89b35996828e0468fb
THOMAS\Thomas <thomas.winkler121@gmail.com>
2017-06-25 15:27:45 +0200

    initial commit

--- src/primeServer.java
+++ src/primeServer.java
@@ -1,33 +1,3 @@
+public class primeServer{
 
-package com.tutego.insel.ws;
-
-import javax.jws.*;
-import javax.jws.soap.SOAPBinding;
-import javax.swing.JOptionPane;
-import javax.xml.ws.Endpoint;
-
-
-@WebService(name = "VerteilteSysteme")
-@SOAPBinding(style = SOAPBinding.Style.RPC)
-public class primeServer {
-
-    @WebMethod
-    public String hello(String name) {
-        return "Hello " + name + "!";
-    }
-
-    @WebMethod(operationName = "body-mass-index")
-    @WebResult(name = "your-bmi")
-    public double bmi(@WebParam(name = "height") double height,
-                      @WebParam(name = "weight") double weight) {
-        return weight / ((height * height) / 10000);
-    }
-
-
-    public static void main(String[] args) {
-        Endpoint prime = Endpoint.publish("http://localhost:8080/services",
-                new primeServer());
-        JOptionPane.showMessageDialog(null, "Server beenden");
-        prime.stop();
-    }
 }
