67826b16c1128242ef4b0467ec00d51ff84f8dc9
Jan Schmutz <janschmutz@hotmail.de>
2017-06-25 18:32:35 +0200

    Final

--- src/wsserver/primeService.java
+++ src/wsserver/primeService.java
@@ -12,26 +12,20 @@ public class primeService {
     /**
      * @param args the command line arguments
      */
-    public void Primzahl() {
+    public String Primzahl() {
         // TODO code application logic here
         boolean abbruch;
-        int primeCount = 0;
+        int primeCount = 50;
+        String str="";
 
-        for (int i = 2; i < 400000; i++) {
-            if (i % 2 != 0) {                  //Wenn Zahl ungerade, weitermachen
-                abbruch = false;
-                for (int j = 3; j < i / 2; j += 2) //Zahl i mod alle Ã¼brigen Teiler (grÃ¶ÃŸer als i/2 reicht), i+=2, damit nur noch ungerade Zahlen ueberprueft werden
-                {
-                    if (i % j == 0) {          //Wenn i%j mindestens ein Mal == 0, ist Zahl i keine Primzahl
-                        abbruch = true;
-                        break;
-                    }
-                }
-                if (!abbruch) {            //Kein Teiler gefunden, also ist Zahl i eine Primzahl
-                    System.out.println(solutionString.add(++primeCount,i));
-                    //System.out.println(i);
-                }
+        for(int i=2;primeCount>0;i++)
+        {
+            if(!new String(new char[i]).matches("(..+?)\\1+"))
+            {
+                str=solutionString.add(primeCount,i,str);
+                primeCount--;
             }
         }
+        return str;
     }
 }
--- src/wsserver/solutionString.java
+++ src/wsserver/solutionString.java
@@ -8,19 +8,12 @@ public class solutionString {
      * @param prim Primzahl
      * return  String
      */
-    static String add(int i, int prim) {
-        return i + ". Primzahl: " + prim;
+    static String add(int i, int prim, String str) {
+        i=51-i;
+        return str + i + ". Primzahl: " + prim + "\n";
     }
 
     /**
      * Main zum aufrufen der Funktion
      */
-    public static void main(String[] args) {
-        int anzahl = 4;
-        int prim[] = {2, 3, 5, 7, 11};
-        for (int i = 0; i <= anzahl; i++) {
-            System.out.println(add(i + 1, prim[i]));
-
-        }
-    }
 }
