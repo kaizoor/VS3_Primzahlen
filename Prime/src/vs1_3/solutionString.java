299a6ce47bd6125751820f50419b3ea91d158b88
juunior <cwolf6@gmx.com>
2017-06-25 14:10:11 +0200

    Kommentare in solutionString, anzahl in i geaendert, klassenname klein geschrieben

---
+++ Prime/src/vs1_3/solutionString.java
@@ -0,0 +1,30 @@
+package vs1_3;
+
+/**
+ * solutionString Klasse um die Ausgabe zusammen zu bauen
+ */
+public class solutionString {
+
+    /*
+     * add Methode zum erzeugen der Ausgabe
+     *
+     * @param i Position der Primzahl
+     * @param prim Primzahl
+     * return  String
+     */
+    private static String add(int i, int prim) {
+        return i + ". Primzahl: " + prim;
+    }
+
+    /**
+     * Main zum aufrufen der Funktion
+     */
+    public static void main(String[] args) {
+        int anzahl = 4;
+        int prim[] = {2, 3, 5, 7, 11};
+        for (int i = 0; i <= anzahl; i++) {
+            System.out.println(add(i + 1, prim[i]));
+        }
+    }
+}
+
