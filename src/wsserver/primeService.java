2ffc157ce6a50725cd1821378d6d360bed4e3fe1
kaizoor <kai.schultz9612@gmail.com>
2017-06-26 10:35:24 +0200

    fix some logik issues

--- src/wsserver/primeService.java
+++ src/wsserver/primeService.java
@@ -17,7 +17,7 @@ public class primeService {
         String str = "";
         int primeCount = 0;
 
-        for (int i = 2; i < 400000; i++) {
+        for (int i = 2; primeCount < target; i++) {
             if (i % 2 != 0) {                  //Wenn Zahl ungerade, weitermachen
                 abbruch = false;
                 for (int j = 3; j < i / 2; j += 2) //Zahl i mod alle Ã¼brigen Teiler (grÃ¶ÃŸer als i/2 reicht), i+=2, damit nur noch ungerade Zahlen ueberprueft werden
